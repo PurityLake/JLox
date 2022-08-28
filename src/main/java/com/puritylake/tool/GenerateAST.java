7ea3c48b169fd11e8ea7175798370ab9b6ad0896
Robert O'Shea <robertoshea2k11@gmail.com>
2022-08-28 19:11:19 +0100

    Updated GenerateAST

--- src/main/java/com/puritylake/tool/GenerateAST.java
+++ src/main/java/com/puritylake/tool/GenerateAST.java
@@ -13,6 +13,7 @@ public class GenerateAST {
             System.err.println("USage: generate_ast <output_directory>");
         }
         String outputDir = args[0];
+
         defineAst(outputDir, "Expr", Arrays.asList(
                 "Binary    : Expr left, Token operator, Expr right",
                 "Grouping : Expr expression",
@@ -36,21 +37,38 @@ public class GenerateAST {
         writer.println();
         writer.println("abstract class " + baseName + " {");
 
+        defineVisitor(writer, baseName, types);
+
         for (String type : types) {
             String className = type.split(":")[0].trim();
             String fields = type.split(":")[1].trim();
             defineType(writer, baseName, className, fields);
         }
 
+        writer.println();
+        writer.println("    abstract <R> R accept(Visitor<R> visitor);");
+
         writer.println("}");
         writer.close();
+    }
+
+    private static void defineVisitor(PrintWriter writer, String baseName, List<String> types) {
+        writer.println("    interface Visitor<R> {");
+
+        for (String type : types) {
+            String typeName = type.split(":")[0].trim();
+            writer.println("        R visit" + typeName + baseName + "(" +
+                    typeName + " " + baseName.toLowerCase() + ");");
+        }
+
+        writer.println("    }");
     }
     
     private static void defineType(
             PrintWriter writer, String baseName,
             String className, String fieldList) {
         writer.println("    static class " + className + " extends " +
-                baseName + "{");
+                baseName + " {");
         
         // constructor
         writer.println("        " + className + "(" + fieldList + ") {");
@@ -61,6 +79,12 @@ public class GenerateAST {
             String name = field.split(" ")[1];
             writer.println("            this." + name + " = " + name + ";");
         }
+        writer.println("        }");
+
+        writer.println();
+        writer.println("        @Override");
+        writer.println("        <R> R accept(Visitor<R> visitor) {");
+        writer.println("            return visitor.visit" + className + baseName + "(this);");
         writer.println("        }");
 
         // Fields
