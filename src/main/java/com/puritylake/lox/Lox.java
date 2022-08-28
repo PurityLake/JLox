67afaeac5144bc188ea6816538e520d13788daf8
Robert O'Shea <robertoshea2k11@gmail.com>
2022-08-28 19:31:49 +0100

    Added AstPrinter

--- src/main/java/com/puritylake/lox/Lox.java
+++ src/main/java/com/puritylake/lox/Lox.java
@@ -12,6 +12,16 @@ public class Lox {
     private static boolean hadError = false;
 
     public static void main(String[] args) throws IOException {
+        Expr expression = new Expr.Binary(
+                new Expr.Unary(
+                        new Token(TokenType.MINUS, "-", null, 1),
+                        new Expr.Literal(123)),
+                new Token(TokenType.STAR, "*", null, 1),
+                new Expr.Grouping(
+                        new Expr.Literal((45.67))));
+
+        System.out.println(new AstPrinter().print(expression));
+
         if (args.length > 1) {
             System.out.println("Usage: jlox [script]");
             System.exit(64);
